db766b18c33207307ec068e83a234f21fca648a4
lesnikovskiy <lesnikovski@gmail.com>
2024-08-16 09:34:40 +0300

    Implement basic Stack struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod basket;
+mod stack;
 
 use basket::Basket;
+use stack::Stack;
 
 fn main() {
     let b1 = Basket::new(String::from("hi there"));
     let b2 = Basket::new(10);
     let b3 = Basket::new(true);
+
+    let st1 = Stack::new(vec![String::from("hi")]);
 }
